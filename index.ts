92fa30cef289ed3053f6b5678e07848a6d9eb986
tsar-boomba <77396670+tsar-boomba@users.noreply.github.com>
2021-11-20 13:40:53 -0500

    it works

---
+++ index.ts
@@ -0,0 +1,40 @@
+import playwright from 'playwright';
+import fs from 'fs';
+import Path from 'path';
+import axios from 'axios';
+
+const fileExtensions = ['jpg', 'jpeg', 'png', 'svg', 'gif'];
+
+const getFileExtension = (url: string) => {
+	let ext = '';
+	fileExtensions.forEach((extension) => {
+		if (url.toLowerCase().includes(extension)) {
+			ext = extension;
+		}
+	});
+	return ext;
+};
+
+const getImages = async (url: string) => {
+	const browser = await playwright.chromium.launch({ headless: true });
+	const page = await browser.newPage();
+	await page.goto(url);
+	const images = await page.$$eval('img', (selected) => {
+		return selected.map((img) => ({ src: img.src, alt: img.alt }));
+	});
+	fs.mkdir(`${__dirname}/output`, () => {
+		images.forEach(async (img, index) => {
+			const path = Path.resolve(
+				__dirname,
+				'output',
+				`${img.alt || `img${index}`}.${getFileExtension(img.src)}`
+			);
+			const writer = fs.createWriteStream(path);
+			const res = await axios.get(img.src, { responseType: 'stream' });
+			res.data.pipe(writer);
+		});
+	});
+	await browser.close();
+};
+
+getImages('https://allenuniversity.edu/');
